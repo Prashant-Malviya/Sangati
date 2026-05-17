bcb7ec854ab09a5e7649ac1345e2b1437c0d3863
Prashant <prashantmalviya272002@gmail.com>
2026-05-17 06:02:35 +0530

    first example of useref

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,26 @@
+import { useRef } from "react";
+
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+
+const videoRef = useRef<HTMLVideoElement | null>(null);
+
+const test = ()=>{
+ 
+  if(videoRef.current)
+  {
+    const player = videoRef.current;
+    player.src = "https://www.w3schools.com/Html/mov_bbb.mp4"
+    player.play()
+  }
 }
 
-export default Home
+  return (
+    <div>
+      Home
+      <video ref={videoRef} id="video" controls width={720} />
+      <button onClick={test}>Test</button>
+    </div>
+  );
+};
+
+export default Home;
